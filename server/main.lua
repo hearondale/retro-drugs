54bf95ac3892ac8dfa8fe35577512b5ca32e1f38
hearondale <mark.drenovv@gmail.com>
2024-06-20 18:46:29 +0200

    bugfix

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,9 @@
 local ox_inventory = exports.ox_inventory
 
 lib.callback.register('drugs:sellToNPC', function(source, itemname, cnt)
-    local source = source
-
     local result = ox_inventory:RemoveItem(source, itemname, cnt + 1)
     if result then
-        ox_inventory:AddItem(source, 'money', Config.Price[itemname])
+        ox_inventory:AddItem(source, 'money', Config.Price[itemname] * (cnt + 1))
     else
         lib.notify(source, {
             title = 'Bad luck',
