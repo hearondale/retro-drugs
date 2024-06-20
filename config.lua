1aac117f997ac26170c55a7eea6b59bf041f6c5d
hearondale <128739748+hearondale@users.noreply.github.com>
2024-06-20 16:44:47 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,14 +3,14 @@ Config = {}
 Config.Debug = false
 
 Config.maxDistToSell = 2
-Config.timeOfSelling = 3000 -- In ms
+Config.timeOfSelling = 7000 -- In ms
 
-Config.chanceToSell = 40 -- in percent
-Config.maxAmountToSell = 3
+Config.chanceToSell = 30 -- in percent
+Config.maxAmountToSell = 2
 
 Config.chanceToCallCops = 25 -- in percent
 
-Config.Price = {
+Config.Price = { -- Price for one
     ['weed_baggy'] = 51,
     ['lsd_mark'] = 123
 }
